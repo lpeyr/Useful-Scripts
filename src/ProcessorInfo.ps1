585f05813592f3394f0512e3853a573051a520d9
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-12-26 18:01:41 +0100

    Create ProcessorInfo.ps1

---
+++ src/ProcessorInfo.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module 'Microsoft.PowerShell.Management'
+
+$processor = Get-WmiObject -Class "Win32_Processor"
+Write-Host "Manufacturer: $($processor.Manufacturer)"
+Write-Host "Architecture: $($processor.AddressWidth)"
+Write-Host "Name: $($processor.Name)"
+Write-Host "Number of Cores: $($processor.NumberOfCores)"
+Write-Host "Number of Logical Processors: $($processor.NumberOfLogicalProcessors)"
