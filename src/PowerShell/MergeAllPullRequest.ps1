36a1ee5df1a60517f728aef94960aed9226c2cf2
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-10-12 12:48:10 +0200

    Replaced  "&&" by ";"

--- src/PowerShell/MergeAllPullRequest.ps1
+++ src/PowerShell/MergeAllPullRequest.ps1
@@ -2,5 +2,5 @@ function merge
 {
     $prIds = (gh pr list --state open --json number --jq '.[] | .number') -join ","
     $reviewCommands = $prIds -split "," | ForEach-Object { "gh pr merge $_" }
-    $reviewCommands -join " && "
+    $reviewCommands -join ";"
 }
--- src/PowerShell/ReviewAllPullRequest.ps1
+++ src/PowerShell/ReviewAllPullRequest.ps1
@@ -2,5 +2,5 @@ function review
 {
     $prIds = (gh pr list --state open --json number --jq '.[] | .number') -join ","
     $reviewCommands = $prIds -split "," | ForEach-Object { "gh pr review -a $_" }
-    $reviewCommands -join " && "
+    $reviewCommands -join ";"
 }
