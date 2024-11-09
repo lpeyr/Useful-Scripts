2f1e5ec2b4e946a6e800d98b2cafa3ad9d175cdc
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-11-09 17:14:56 +0100

    Updated merge command

--- src/Bash/MergeCommandPR.sh
+++ src/Bash/MergeCommandPR.sh
@@ -1,6 +1,6 @@
 merge() {
     pr_ids=$(gh pr list --state open --json number --jq '.[] | .number')
     for pr_id in $pr_ids; do
-        gh pr merge "$pr_id" --merge -d
-    done
+        echo "gh pr merge $pr_id --merge -d"
+    done | paste -sd ";"
 }
